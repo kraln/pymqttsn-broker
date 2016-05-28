4079a621e3fbac9bd7b08b63a2627f062cf6db7a
Jeff Katz <jeff.katz@kiwi.ki>
2016-05-29 01:48:27 +0200

    Add MQTTSN Message and Flag class
    Add classes to represent MQTTSN-Messages and Flags.
    
    These are going to get broken out into their own files (for /sure/),
    but for now they live in the broker.
    
    I can parse CONNECT messages now :)

--- broker/broker.py
+++ broker/broker.py
@@ -3,6 +3,147 @@ import logging
 
 logger = logging.getLogger('broker.socketio')
 
+
+class MQTTSNFlags:
+    """
+    Flags class
+
+    Represents the flag byte on messages
+
+    fields:
+        dup
+        qos
+        retain
+        will
+        clean_session
+        topic_id_type
+    """
+    def __init__(self, flags):
+        self.dup = True if flags & 0b10000000 else False
+        self.qos = (flags & 0b01100000) >> 5
+        self.retain = True if flags & 0b00010000 else False
+        self.will = True if flags & 0b00001000 else False
+        self.clean_session = True if flags & 0b00000100 else False
+        self.topic_id_type = flags & 0b00000011
+
+class MQTTSNMessage:
+    """
+    Message class
+
+    Defined by Chapter 5 of the spec.
+
+    I have a length, and a message type, and a variable part which depends
+    on the message type.
+    """
+
+    MESSAGE_TYPES = {
+            0x00: 'ADVERTISE',
+            0x01: 'SEARCHGW',
+            0x02: 'GWINFO',
+            0x04: 'CONNECT',
+            0x05: 'CONNACK',
+            0x06: 'WILLTOPICREQ',
+            0x07: 'WILLTOPIC',
+            0x08: 'WILLMSGREQ',
+            0x09: 'WILLMSG',
+            0x0A: 'REGISTER',
+            0x0B: 'REGACK',
+            0x0C: 'PUBLISH',
+            0x0D: 'PUBACK',
+            0x0E: 'PUBCOMP',
+            0x0F: 'PUBREC',
+            0x10: 'PUBREL',
+            0x12: 'SUBSCRIBE',
+            0x13: 'SUBACK',
+            0x14: 'UNSUBSCRIBE',
+            0x15: 'UNSUBACK',
+            0x16: 'PINGREQ',
+            0x17: 'PINGRESP',
+            0x18: 'DISCONNECT',
+            0x1A: 'WILLTOPICUPD',
+            0x1B: 'WILLTOPICRESP',
+            0x1C: 'WILLMSGUPD',
+            0x1D: 'WILLMSGRESP',
+            0xFE: '(encapsulated)'
+    }
+
+    def __str__(self):
+        if self.message_type == 0x4:
+            # CONNECT
+            return "%s: W?%d C?%d Keepalive %d ID: %s" % (
+                    self.MESSAGE_TYPES[self.message_type],
+                    self.flags.will,
+                    self.flags.clean_session,
+                    self.duration,
+                    self.client_id)
+
+    def parse(self, data):
+        """Parse bytes into a struct"""
+
+        # Data validation?
+        if len(data) < 2:
+            logger.warning('Invalid packet, discarding')
+            return False
+
+        try:
+            # Determine length (big message support)
+            if data[0] == 1:
+                self.length = data[1] << 8 | data[2]
+                self.message_type = data[3]
+                self.variable_index = 4
+            else:
+                self.length = data[0]
+                self.message_type = data[1]
+                self.variable_index = 2
+
+            logger.debug('Packet %x (%s) decoded, length %x' % (
+                self.message_type,
+                self.MESSAGE_TYPES[self.message_type],
+                self.length,
+                )
+            )
+
+        except Exception as e:
+            # should I rethrow here?
+            print(e)
+            return False
+
+        # Now, message-specific parsing
+
+        if self.message_type == 0x4:
+            # CONNECT
+            """
+            Flags:
+                NOT USED: DUP, QoS, Retain, TopicIDType
+                Will: If set, client requests will topic and message prompting
+                CleanSession: If set, remove all subscriptions for this client
+            ProtocolID:
+                Must be always 0x1
+            Duration:
+                Keep alive timer duration
+            Client ID:
+                (Rest of the message) The client's ID
+            """
+            self.flags = MQTTSNFlags(data[2])
+
+            # This has to be 1, basically.
+            if data[3] != 1:
+                logger.warning("Unknown protocol id: %d", (data[3],))
+                return false
+
+            self.duration = data[4] << 8 | data[5]
+            self.client_id = data[6:]
+
+        elif self.message_type == 0x16:
+            # PINGREQ
+            pass
+        elif self.message_type == 0x0C:
+            # PUBLISH
+            pass
+
+        # I guess everything's okay
+        return True
+
 class MQTTSNBrokerProtocol:
 
     def connection_made(self, transport):
@@ -11,7 +152,12 @@ class MQTTSNBrokerProtocol:
 
     def datagram_received(self, data, addr):
         logger.debug(('Data received:', data, addr,))
-        self.transport.sendto(data, addr)
+        msg = MQTTSNMessage()
+
+        if msg.parse(data):
+            logger.debug('Parsed: %s' % (msg, ))
+        else:
+            logger.warning('Message parse error!')
 
     def error_received(self, exc):
         logger.error(('Error received:', exc,))
